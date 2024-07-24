a476b09701dc62021b18483b35dfa284c5667759
Noahabebe <73600593+Noahabebe@users.noreply.github.com>
2024-07-24 11:27:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const legoData = require("./modules/legoSets");
 
 app.set('view engine', 'ejs');
 
+app.set('views', __dirname + '/views');
 
 app.use(express.static(path.join(__dirname, "public")));
 
